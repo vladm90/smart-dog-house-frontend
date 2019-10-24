cdcc8536d2233bde63f1dc3faaa9cc79a2c9d885
vladm <vladm90@yahoo.com>
2019-10-24 23:39:15 +0300

    initial commit

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = 'http://localhost:8080/temperature';
+const USER_API_BASE_URL = 'http://localhost:8080/api/temperatures';
 
 class ApiService {
 
